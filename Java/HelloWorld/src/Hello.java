f8fb94b68f1526afd02baf8676e83337dd46f514
driptanil <driprecovery@gmail.com>
2021-09-04 22:13:18 +0530

    commit to main

--- Java/HelloWorld/src/Hello.java
+++ Java/HelloWorld/src/Hello.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Hello {
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Driptanil");
+
+        int myFirstNumber=(10+5)+(10*2),mySecondNumber=12,myThirdNumber=myFirstNumber*2;
+        int myTotal=myFirstNumber+mySecondNumber+myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne=1000-myTotal;
+        System.out.println(myLastOne);
+    }
 }
