09fed1eddd3a04ffdf8f3d6aaca42bee827b4506
driptanil <driprecovery@gmail.com>
2021-09-24 14:46:01 +0000

    commit to main

--- Java/03-conditionals-loops/src/com/assignments/HcfLcm.java
+++ Java/03-conditionals-loops/src/com/assignments/HcfLcm.java
@@ -11,36 +11,22 @@ public class HcfLcm {
         int num2=in.nextInt();
         int lcm;
         int hcf=1;
-        int large;
-        boolean bool=true;
+        int large,small;
         if(num1>num2){
             large=num1;
+            small=num2;
         }
         else{
             large=num2;
+            small=num1;
         }
-        for (int i=2;i<=large;i++) {
-            for (int j=2;j<i;j++) {
-                if(i%j==0){
-                    bool=false;
-                }
-            }
-            if(bool==true){
-                int num1_copy=num1;
-                int num2_copy=num2;
-                while (true) {
-                    if (num1_copy % i == 0 && num2_copy % i == 0) {
-                        num1_copy/=i;
-                        num2_copy/=i;
-                        hcf *= i;
-                    }
-                    else{
-                        break;
-                    }
-                }
+        for (int index = small; index > 0; index--) {
+            if (large%index==0 && small%index==0){
+                hcf=index;
+                break;
             }
         }
-        lcm=num1*num2/hcf;
+        lcm=(int)(num1*num2/hcf);
         System.out.println("HCF: "+hcf);
         System.out.println("LCM: "+lcm);
     }
