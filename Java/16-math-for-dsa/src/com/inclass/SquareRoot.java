77337a5e0f2efb5f09ecfdc60324fb090cc68084
driptanil <driprecovery@gmail.com>
2022-02-03 15:30:22 +0530

    math for dsa

--- Java/16-math-for-dsa/src/com/inclass/SquareRoot.java
+++ Java/16-math-for-dsa/src/com/inclass/SquareRoot.java
@@ -3,50 +3,39 @@ package com.inclass;
 public class SquareRoot {
     public static void main(String[] args) {
         int n = 40;
-        System.out.println(precision(n, (double)root(n)));
+        int p = 5;
+        System.out.println(root(n, p));
     }
 
-    static int root (int n) {
-        int start = 0;
-        int end = n - 1;
+    static double root (int n, int p) {
+        int start = 1;
+        int end = n;
+        double root = 0;
         while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (n / (mid + 1) == mid + 1) {
-                return mid + 1;
-            } else if (n / (mid + 1) > mid + 1) {
-                start = mid + 1;
+            int m = start + (end - start) / 2;
+            if (m * m == n) {
+                return m;
+            } else if (m > n) {
+                start = m + 1;
             } else {
-                end = mid - 1;
+                end = m - 1;
+                root = m;
             }
         }
-        return start + (end - start) / 2 + 1;
+
+        double decimal = 0.1;
+        for (int i = 0; i < p; i++) {
+            while (root * root <= n) {
+                root += decimal;
+            }
+            root -= decimal;
+            decimal /= 10;
+        }
+
+        return root;
     }
 
-    static double precision (int n, double sol) {
-        float decimal = 0.1f;
-        int count = 0;
-        while (count < 6) {
-            int start = 0;
-            int end = 9;
-            double temp = 0;
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-                temp = sol + decimal * mid;
-                if (n / temp == temp) {
-                    return temp;
-                } else if (n / temp > temp) {
-                    if (n / (temp + decimal) < temp) {
-                        decimal *= 0.1;
-                        sol = temp;
-                        break;
-                    }
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            }
-            count += 1;
-        }
-        return sol;
+    static double newtonRaphson (int n, int p) {
+        
     }
 }
