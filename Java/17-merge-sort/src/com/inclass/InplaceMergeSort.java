d5b7d7186b932e82022cfb1f3529eb06338c26a0
driptanil <driprecovery@gmail.com>
2022-02-20 21:00:15 +0530

    Merge and Quick Sort

--- Java/17-merge-sort/src/com/inclass/InplaceMergeSort.java
+++ Java/17-merge-sort/src/com/inclass/InplaceMergeSort.java
@@ -4,30 +4,35 @@ import java.util.Arrays;
 
 public class InplaceMergeSort {
     public static void main(String[] args) {
-        int[] arr = {6, 5, 4, 3, 2, 1};
-        merge(arr, 0 , arr.length - 1);
+        int[] arr = {5, 4, 1, 2, 3};
+        mergeSort(arr, 0 , arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
     static void mergeSort (int[] arr, int start, int end) {
-        if (end - start == 1) {
-            merge(arr, start, end);
+        if (start >= end) {
             return;
         }
-        int mid = start + (end - start) / 2;
+        int mid = start + (end - start + 1) / 2;
         mergeSort(arr, start, mid - 1);
         mergeSort(arr, mid, end);
+        merge(arr, start, end);
     }
 
     static void merge (int[] arr, int start, int end) {
-        int i = start;
-        while (i < end) {
-            if (arr[i] < arr[i - 1]) {
-                int temp = arr[i - 1];
-                arr[i - 1] = arr[i];
-                arr[i] = temp;
+        int mid = start + (end - start + 1) / 2;
+        while (start < mid && mid <= end) {
+            if (arr[start] > arr[mid]) {
+                for (int i = mid - 1; i >= start; i--) {
+                    int temp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp;
+                }
+                start++;
+                mid++;
+            } else {
+                start++;
             }
-            i++;
         }
     }
 }
