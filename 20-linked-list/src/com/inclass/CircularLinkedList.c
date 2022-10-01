5b41b93aef8c54c9a791f5787187eada63542fe4
Ritam Chatterjee <94624665+Ritam135@users.noreply.github.com>
2022-10-01 23:33:00 +0530

    Update CircularLinkedList.c

---
+++ 20-linked-list/src/com/inclass/CircularLinkedList.c
@@ -0,0 +1,155 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+    int val;
+    struct node *next;
+}node;
+node *head=NULL;
+node *init(int ele)
+{
+    node *temp;
+    temp=(node*)malloc(sizeof(node));
+    temp->val=ele;
+    temp->next=NULL;
+    return temp;
+}
+void insertFirst()
+{
+   node *ptr,*current;
+   int i;
+   printf("Enter the new element");
+   scanf("%d",&i);
+   ptr=(init(i));
+   printf("%d",ptr->val);
+   if(head==NULL)
+   {
+      head=ptr;
+      ptr->next=head;
+   }
+   else
+   {
+      current=head;
+      while(current->next!=head)
+       current=current->next;
+      ptr->next=head;
+      current->next=ptr;
+      head=ptr;
+   }
+}
+void insertLast()
+{
+   node *ptr,*current;
+   int i;
+   printf("Enter the new element");
+   scanf("%d",&i);
+   ptr=init(i);
+   current=head;
+   if(head==NULL)
+   {
+      head=ptr;
+      ptr->next=head;
+   }
+   else
+   {
+     while(current->next!=head)
+       current=current->next;
+     current->next=ptr;
+     ptr->next=head;
+   }
+}
+void deleteFirst()
+{ 
+    node *ptr,*current;
+    ptr=head;
+    if(head==NULL)
+      printf("Empty Linked List !!!");
+    else if(ptr->next==head)
+    {
+        printf("%d",ptr->val);
+        head=NULL;
+        free(ptr);
+    }
+    else
+    {
+     ptr=head;
+     current=head;
+     while(current->next!=head)
+      current=current->next;
+     current->next=ptr->next;
+     head=ptr->next;
+     printf("%d",ptr->val);
+     free(ptr);
+    }
+}
+void deleteLast()
+{
+    node *current,*prev;
+    current=head;
+    if(head==NULL)
+      printf("Empty Linked List !!!");
+    else if(current->next==head)
+    {
+        printf("%d",current->val);
+        head=NULL;
+        free(current);
+    }
+    current=head;
+    while(current->next!=head)
+    {
+      prev=current;
+      current=current->next;
+    }
+    printf("%d\n",current->val);
+    prev->next=current->next;
+    free(current);
+}
+void display()
+{
+    node *ptr;
+    if(head==NULL)
+      printf("NULL");
+    else
+    {
+    ptr=head;
+    while(ptr->next!=head)
+    {
+      printf("%d->",ptr->val);
+      ptr=ptr->next;
+    }
+    printf("%d",ptr->val);
+    }
+}
+int main()
+{
+    int choice;
+    while(1)
+    {
+      printf("\n 1. insertFirst 2.insertLast 3.deleteFirst 4.deleteLast 5.display 6.exit");
+      printf("\n Enter the choice");
+      scanf("%d",&choice);
+      switch(choice)
+      {
+        case 1:
+           insertFirst();
+           break;
+        case 2:
+           insertLast();
+           break;
+        case 3:
+           deleteFirst();
+           break;
+        case 4:
+           deleteLast();
+           break;
+        case 5:
+           display();
+           break;
+        case 6:
+           return 0;
+        default:
+           printf("Enter valid choice");
+      }
+    }
+    return 0;
+}
