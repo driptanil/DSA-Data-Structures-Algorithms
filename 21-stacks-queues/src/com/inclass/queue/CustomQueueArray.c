460410c1a439db5648276087b8b088df065bb021
Vihanga <vihangamara@gmail.com>
2022-10-01 13:47:50 +0530

    adding queue implementation using array in c

---
+++ 21-stacks-queues/src/com/inclass/queue/CustomQueueArray.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include <stdlib.h>
+#define size 5
+#define true 1
+#define false 0
+
+int currentSize=0;
+int front=0,rear=-1;
+
+int isFull(){
+    if(currentSize==size){
+        return true;
+    }else return false;
+}
+
+int isEmpty(){
+    if(currentSize==0){
+        return true;
+    }else return false;
+}
+
+void enqueue(int * arr,int value){
+    if (!isFull()){
+        rear=(rear+1)%size;
+        arr[rear]=value;
+        currentSize++;
+    }else{
+        printf("%s","Queue is full\n");
+    }
+}
+
+int dequeue(int * arr){
+    if (!isEmpty()){
+       int temp =arr[front];
+       front=(front+1)%size;
+       currentSize--;
+    }else{
+        printf("%s","Queue is empty\n");
+    }
+}
+
+void display(int * arr){
+    if (!isEmpty()){
+            for(int i =0;i<currentSize;i++){
+               if(i==0){
+                    printf("[%d",arr[i]);
+                } else printf(",%d",arr[i]);
+
+            }
+            printf("]\n");
+    }else{
+        printf("%s","Null\n");
+    }
+}
+
+void main(){
+
+    int * arr = (int *)malloc(sizeof(int)*size);
+
+    while(1) {
+        int selection;
+
+        printf("%s","***Queue Operation Menu*** \n1. Enqueue 2. Dequeue 3.Display 4. Exit\n");
+        printf("%s","Enter your selection: ");
+        scanf("%d",&selection);
+
+        switch(selection){
+            int value;
+            case(1):
+
+                    printf("%s","Enter value to be inserted: ");
+                    scanf("%d",&value);
+                    enqueue(arr,value);
+                    break;
+
+            case(2):dequeue(arr);
+                    break;
+            case(3):
+                    display(arr);
+                    break;
+            case(4):
+                printf("%s","Exited successfully");
+                return;
+            default:
+                printf("%s","Invalid selection\n");
+        }
+
+   }
+
+
+}
